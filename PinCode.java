75352f78321cee5016bfe4766f0d37e5473c38b8
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-12-02 15:03:35 +0530

    PinCode UC4

--- PinCode.java
+++ PinCode.java
@@ -3,8 +3,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class PinCode {
     public static void main(String[] args) {
-        String postalCode = "400088B";
-        Pattern pattern = Pattern.compile("[4]?[0]{3}[8]{2}[![#%&_:;'/|][^a-zA-Z]]$");
+        String postalCode = "400 088";
+        Pattern pattern = Pattern.compile("[4]?[0]{2}[\\s]*[0]?[8]{2}");
         Matcher matcher = pattern.matcher(postalCode);
         System.out.println(matcher.find());
     }
