dab1046788e49520f8908ed7755b0e313b8aa7cb
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-12-02 14:58:38 +0530

    PinCode UC1

--- PinCode.java
+++ PinCode.java
@@ -1,1 +1,11 @@
-
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class PinCode {
+    public static void main(String[] args) {
+        String postalCode = "400088";
+        Pattern pattern = Pattern.compile("[4]?[0]{3}[8]{2}");
+        Matcher matcher = pattern.matcher(postalCode);
+        System.out.println(matcher.find());
+    }
+}
